6ca9fd6e4272d5dfc7ace8e9406764c438e5fb25
soyocar <royomek@gmail.com>
2022-05-05 15:43:45 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+let btnEnv = document.getElementById("enviar");
+let base = document.getElementById("base");
+let altura = document.getElementById("altura");
+//let rotulo = document.getElementById("rotulo");
+btnEnv.addEventListener("click", () => {
+  // TU CODIGO VA AQUI, POR EJEMPLO:
+  console.log("El area del rectángulo es  ", base * altura);
+  //}
+});
